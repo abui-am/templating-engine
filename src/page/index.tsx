a3249ccf43be142427ce6a5858596e9b82cbedf3
abui <adjiem31@gmail.com>
2024-05-26 08:44:35 +0700

    feat: poc

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -1,7 +1,158 @@
-import CommonLayout from '../components/layouts/common';
+import base from '../assets/base.svg';
+import React from 'react';
+import Input from '../components/common/input';
+import Highlighter from 'react-highlight-words';
+import style from './index.module.css';
+import { cx } from '../helpers';
+import Button from '../components/common/button';
 
+import * as htmlToImage from 'html-to-image';
+
+const MAX_WIDTH = 600;
 function IndexPage() {
-  return <CommonLayout>Index Page</CommonLayout>;
+  const mainContentRef = React.useRef<HTMLDivElement>(null);
+  const [contentWidth, setContentWidth] = React.useState<number>(0);
+  React.useEffect(() => {
+    if (mainContentRef.current) {
+      setContentWidth(mainContentRef.current.offsetWidth);
+    }
+  }, [mainContentRef.current]);
+  // Reset width and height on resize
+  React.useEffect(() => {
+    const onResize = () => {
+      setContentWidth(0);
+    };
+    window.addEventListener('resize', onResize);
+    return () => {
+      window.removeEventListener('resize', onResize);
+    };
+  }, []);
+  const actualWidth = 1080;
+
+  const scaledWidth = (width: number) => (contentWidth / actualWidth) * width;
+
+  const fontSize = scaledWidth(48);
+  const fontSizeSource = scaledWidth(24);
+
+  const [text, setText] = React.useState<string>('');
+  const [highlight, setHighlight] = React.useState<string>('');
+  const [backgroundUrl, setBackgroundUrl] = React.useState<string>('');
+  const [source, setSource] = React.useState<string>('');
+  const handleSaveImage = () => {
+    console.log('save image', mainContentRef.current);
+    if (!mainContentRef.current) {
+      return;
+    }
+
+    htmlToImage.toPng(mainContentRef.current).then(function (dataUrl) {
+      const link = document.createElement('a');
+      link.download = `template-${Date.now()}.png`;
+      link.href = dataUrl;
+      link.click();
+    });
+  };
+
+  return (
+    <div
+      style={{
+        maxWidth: MAX_WIDTH,
+        padding: '20px 0px',
+      }}
+      className='mx-auto'
+    >
+      <div
+        ref={mainContentRef}
+        style={{
+          maxWidth: MAX_WIDTH,
+          background: `url(${backgroundUrl}) no-repeat center center`,
+        }}
+        className='relative w-full text-white leading-[210%]'
+      >
+        <div
+          style={{
+            fontSize: fontSizeSource,
+            position: 'absolute',
+            top: '45%',
+            right: scaledWidth(24),
+            width: 0,
+            transform: 'rotate(-90deg)',
+            whiteSpace: 'nowrap',
+            lineHeight: 1,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          Sumber: {source}
+        </div>
+        <div
+          style={{
+            fontSize,
+            position: 'absolute',
+            top: scaledWidth(900),
+            left: scaledWidth(60),
+            right: scaledWidth(60),
+          }}
+        >
+          <Highlighter
+            className='font-bold'
+            highlightClassName={cx(style['highlighted'])}
+            searchWords={[highlight]}
+            autoEscape={true}
+            textToHighlight={text}
+          />
+        </div>
+        <img src={base} alt='tmp' className='w-full' />
+      </div>
+
+      <div className='mt-8'>
+        <div>
+          <label>Text</label>
+          <Input
+            onChange={(e) => {
+              setText(e.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <label>Highlight</label>
+
+          <Input
+            onChange={(e) => {
+              setHighlight(e.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <label>sumber</label>
+
+          <Input
+            onChange={(e) => {
+              setSource(e.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <label className='block'>Background</label>
+          <input
+            type='file'
+            onChange={(e) => {
+              const file = e.target?.files?.[0];
+              if (!file) {
+                return;
+              }
+
+              const url = URL.createObjectURL(file);
+              setBackgroundUrl(url);
+            }}
+          />
+        </div>
+        <Button onClick={handleSaveImage} className='mt-6 w-full'>
+          Save Image
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default IndexPage;
